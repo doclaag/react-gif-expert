f2e0ffe7709eaa973f7086d7da4270607a54fd78
Luis Alonzo <doclaag@gmail.com>
2023-10-28 21:54:31 -0600

    Modifications to use environment variables

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,20 +1,29 @@
 
 export const getGifs = async (category) => {
-    // const apiKey = import.meta.env.VITE_API_KEY;
-    const apiKey = process.env.VITE_API_KEY;
-    const limit = 10;
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=${limit}`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
+    try{
 
-    const gifs = data.map((img) => ({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url,
-    }));
+        const apiKey = import.meta.env.VITE_API_KEY;
 
-    // console.log(gifs);
+        const limit = 10;
 
-    return gifs;
+        // console.log(apiKey);
+        
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=${limit}`;
+        const resp = await fetch(url);
+        const { data } = await resp.json();
+        
+        const gifs = data.map((img) => ({
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url,
+        }));
+        
+        // console.log(gifs);
+        
+        return gifs;
+    }catch(error){
+        console.error("Error fetching gifs:", error);
+        throw error; 
+    }
 };
